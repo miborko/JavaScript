45a8cfc65458d1b35672d439425fa9e0e348be82
Michał Borkowski <michal88borkowski@gmail.com>
2018-02-10 16:32:42 +0100

    Zmienne

---
+++ 2-zmienne/js/var.js
@@ -0,0 +1,12 @@
+var imie = "Michal";
+var nazwisko = "Borkowski";
+var wiek = 29;
+var wzrost;
+
+var wynik = "Imię" + imie + " Nazwisko" + nazwisko + " Wiek" + wiek;
+console.log(imie);
+console.log(nazwisko);
+console.log(wiek);
+console.log(wynik); 
+
+console.log(wzrost);
---
+++ Komentarze/js/1-comments.js
@@ -0,0 +1,5 @@
+/*Komentarz blokowy
+Dzieki niemu mozemy nasz komentrz umiescuc w wielu liniach. 
+Skrót kody ctrl + shift + / */
+
+//Komentarz liniowy, skończy się razem z końcem tego wiersza, skrót crtl + / musze napisać długi komentarz żeby się nie zmieścił w jednym wiersz
