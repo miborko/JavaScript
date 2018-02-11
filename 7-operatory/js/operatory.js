c76ba7100659e701f6540acefcc3f5ed38d045d6
Michał Borkowski <michal88borkowski@gmail.com>
2018-02-11 14:37:53 +0100

    Operatory

---
+++ 7-operatory/js/operatory.js
@@ -0,0 +1,52 @@
+var liczba1 = 23;
+var liczba2 = 6;
+
+//console.log( liczba2 * liczba1 );
+//console.log( liczba1 / liczba2 );
+//console.log( liczba1 + liczba2 );
+//console.log( liczba1 - liczba2 );
+//console.log( liczba1 % liczba2 );
+//
+////liczba1++;
+////console.log( liczba1 );
+////liczba1--;
+////console.log( liczba1 );
+//
+//console.log(liczba1++);
+//console.log(++liczba1);
+
+//console.log(liczba1 +=2); //liczba1 = liczba1 + 2
+//console.log(liczba1 -=2); 
+//console.log(liczba1 *=2); 
+//console.log(liczba1 /=2); 
+
+var wynik = (liczba1 >= liczba2) ? "Liczba1 większa od liczba2" : "liczba1 mniejsza od liczba2";
+console.log(wynik);
+
+var wynik = (liczba1 == liczba2) ? "Liczba1 większa od liczba2" : "liczba1 mniejsza od liczba2";
+console.log(wynik);
+
+var wynik = (liczba1 != liczba2) ? "prawda" : "fałsz";
+console.log(wynik);
+
+//var wynik = (liczba1 == liczba2) ? "Liczba1 większa od liczba2" : "liczba1 mniejsza od liczba2";
+//console.log(wynik);
+//
+//var wynik = (liczba1 == liczba2) ? "Liczba1 większa od liczba2" : "liczba1 mniejsza od liczba2";
+//console.log(wynik);
+//
+//var wynik = (liczba1 == liczba2) ? "Liczba1 większa od liczba2" : "liczba1 mniejsza od liczba2";
+//console.log(wynik);
+//
+//var wynik = (liczba1 == liczba2) ? "Liczba1 większa od liczba2" : "liczba1 mniejsza od liczba2";
+//console.log(wynik);
+
+
+
+
+
+
+
+
+
+
