ef3f546020bfad5393cecc9b788216e491b05315
Michał Borkowski <michal88borkowski@gmail.com>
2018-02-11 15:23:26 +0100

    Switch

--- 7-operatory/js/operatory.js
+++ 7-operatory/js/operatory.js
@@ -40,18 +40,58 @@ var liczba2 = 6;
 //    console.log("Spełnione zostały dwa warunki, także TRUE");
 //}
 
-var pietro = 10;
+//var pietro = 10;
+//
+//if (liczba1 > liczba2 && pietro || 14) {
+//    console.log("Spełniony przynajmniej jeden z warunków");
+//}
+//
+//if (liczba1 == liczba2) {
+//    console.log("Liczby są równe");
+//} else if (liczba1 < liczba2) {
+//    console.log("Liczba 1 większa od liczba2");
+//} else if (liczba1 == "Smog") {
+//    console.log("Liczba równa się smog");
+//} else {
+//    console.log("Kod który wykona się gdy żaden z warunków nie zostanie spełniony");    
+//}
 
-if (liczba1 > liczba2 && pietro || 14) {
-    console.log("Spełniony przynajmniej jeden z warunków");
+
+//SWITCH
+
+var grupa = 16;
+
+switch( grupa ){
+    case 10:
+        console.log("Liczba osób w grupie = 10");
+    break;
+    case 12:
+        console.log("Liczba osób w grupie = 12");
+    break;
+    case 14:
+        console.log("Liczba osób w grupie = 14");
+    break;
+    case 16:
+        console.log("Liczba osób w grupie = 16");
+    break;
+    case 18:
+        console.log("Liczba osób w grupie = 18");
+    break;
+    case 20:
+        console.log("Liczba osób w grupie = 20");
+    break;
+    default :
+        console.log("Nie wiadomo ile osb w grupie");
+    break;
 }
 
-if (liczba1 == liczba2) {
-    console.log("Liczby są równe");
-} else if (liczba1 < liczba2) {
-    console.log("Liczba 1 większa od liczba2");
-} else if (liczba1 == "Smog") {
-    console.log("Liczba równa się smog");
-} else {
-    console.log("Kod który wykona się gdy żaden z warunków nie zostanie spełniony");
-}
+
+
+
+
+
+
+
+
+
+
