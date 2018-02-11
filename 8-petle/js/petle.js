202b7d6bd308f7b75c71c31023d71007eb517f45
Michał Borkowski <michal88borkowski@gmail.com>
2018-02-11 16:34:43 +0100

    Pętle

---
+++ 8-petle/js/petle.js
@@ -0,0 +1,55 @@
+//PĘTLE FOR
+
+//var ileRazy = 30;
+
+//for( var i = 0; i < ileRazy; i++ ) {
+//    var iteracja = 1 + i;
+//    console.log("Iteracja pętli nr " + iteracja);
+//}
+
+
+
+//var tablica = ["Audi", "Opel", "Honda", "Mazda", "Nissan"];
+
+//for( var i = 0; i < tablica.length; i++) {
+//    console.log( "Tablica: index = " + i + ", Wartość = " + tablica[i] );
+//}
+
+
+//PĘTLE FOREACH
+
+
+//var tablica = ["Audi", "Opel", "Honda", "Mazda", "Nissan"];
+
+//tablica.forEach( function( elementTablicy, indeksTablicy ) {
+//    console.log( "Indeks: " + indeksTablicy + ", Wartość: " + elementTablicy );
+//} );
+
+//tablica.forEach( function( elementTablicy, indeksTablicy ) {
+//    if( elementTablicy == "Mazda" ) {
+//        console.log( "Indeks: " + indeksTablicy + ", Wartość: " + elementTablicy );
+//    }
+//}
+
+//PĘTLE WHILE
+
+//var iter = 0;
+//while( iter < 20 ) {
+//    console.log( iter );
+//    ++iter;
+//}
+
+
+//PĘTLE DO WHILE
+
+var iter = 20;
+do {
+    console.log (iter);
+    ++iter;
+} while  (iter < 10 );
+
+
+
+
+
+
